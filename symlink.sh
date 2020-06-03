74979c1920bc7ad612ce25add239a89bd4049326
Sam Dhondt <sam.dhondt@codifly.be>
2020-06-03 09:28:19 +0200

    Remove doom config

--- symlink.sh
+++ symlink.sh
@@ -10,10 +10,3 @@ for file in ${FILES_TO_SYMLINK}; do
 	ln -sf "$file" "$target"
 done
 
-DIRECTORIES_TO_SYMLINK=("$(pwd)/.doom.d/")
-
-for dir in "${DIRECTORIES_TO_SYMLINK[@]}"; do
-	target="$HOME/$(basename "$dir")"
-	echo "Symlinking $dir to $target"
-	ln -sf "$dir" "$target"
-done
