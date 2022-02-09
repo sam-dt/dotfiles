4e4bfc1de0191eb8e68cdc9137f17b5043c4a60a
Sam Dhondt <sam.dhondt@codifly.be>
2022-02-09 15:36:04 +0100

    Update install script for M1 macbooks
    Signed-off-by: Sam Dhondt <sam.dhondt@codifly.be>

--- install.sh
+++ install.sh
@@ -3,17 +3,20 @@
 # This script is meant for a new machine to quickly setup and install applications
 # General setup: fish shell with neovim editor
 
+# Note: /usr/local/bin for macOS Intel machines
+HOMEBREW_BIN=/opt/homebrew/bin
+
 # install XCode cli tools
-xcode-select —-install
+xcode-select --install
 
 # install brew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+export PATH="$PATH:$HOMEBREW_BIN/bin"
 
 # === CLI tools === #
 brew install fish \
   node \
-  --HEAD neovim \ # nightly build, required for coc.nvim
-  rg \ # better grep variant
+  rg \
   jq \
   jsonlint \
   fzf \
@@ -22,8 +25,10 @@ brew install fish \
   shellcheck \
   stow
 
-# === Apps === #
-brew install firefox \
+brew install --HEAD neovim \ # nightly build
+
+# === Applications === #
+brew install --cask firefox \
   google-chrome \
   gas-mask \
   insomnia \
@@ -37,7 +42,7 @@ brew install font-fira-code
 # npm global installs
 npm install -g yarn \
   typescript-language-server \
-  vscode-eslint-language-server
+  vscode-langservers-extracted
 
 # Enable key repeat for vscode (useful for vim extension)
 defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false 
@@ -45,4 +50,10 @@ defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false
 defaults delete -g ApplePressAndHoldEnabled     
 
 # Change default shell
-chsh -s /usr/local/bin/fish
+echo "$HOMEBREW_BIN/fish" | sudo tee -a /etc/shell
+chsh -s "$HOMEBREW_BIN/fish"
+
+# Final step: restart shell so it launches fish
+# Run the following command to setup fish path
+# set -U fish_user_paths "$HOMEBREW_BIN" $fish_user_paths
+
