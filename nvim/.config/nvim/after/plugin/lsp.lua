dea24a59130ff8e2c5ef454ed8440eac8e4920a3
Sam Dhondt <sam.dhondt@codifly.be>
2023-08-03 12:30:27 +0200

    (nvim) Add custom mappings for LSP
    Signed-off-by: Sam Dhondt <sam.dhondt@codifly.be>

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -5,3 +5,14 @@ lsp.on_attach(function(client, bufnr)
 end)
 
 lsp.setup()
+
+
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+
+cmp.setup({
+  mapping = {
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+    ['<C-Space>'] = cmp.mapping.complete(),
+  }
+})
