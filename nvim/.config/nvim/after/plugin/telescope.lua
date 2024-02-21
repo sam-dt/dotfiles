dfecdb5501dd3403a31143a595e4759af9ebc40f
Sam Dhondt <sam.dhondt@codifly.be>
2024-02-21 13:50:54 +0100

    (nvim) Add map to explicitly use Telescope references

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -3,7 +3,8 @@ vim.keymap.set('n', '<Leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<Leader>fw', builtin.grep_string, {})
 vim.keymap.set('n', '<Leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<Leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<Leader>fr', builtin.resume, {})
+vim.keymap.set('n', '<Leader>fc', builtin.resume, {})
+vim.keymap.set('n', '<Leader>fr', builtin.lsp_references, { noremap = true, silent = true })
 
 return require('telescope').setup {
   defaults = {
