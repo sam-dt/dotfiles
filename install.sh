c4a34e8fe4d9a8aed748e829ff41ef5d7b5757b9
Sam Dhondt <sam.dhondt@codifly.be>
2024-09-04 21:36:51 +0200

    Remove install stuff from install script (hehe)

--- install.sh
+++ install.sh
@@ -1,44 +1,6 @@
 #!/bin/bash
 
-# This script is meant for a new machine to quickly setup and install applications
-# General setup: fish shell with neovim editor
-
-# Note: /usr/local/bin for macOS Intel machines
-HOMEBREW_BIN=/opt/homebrew/bin
-
-# install XCode cli tools
-xcode-select --install
-
-# install brew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-export PATH="$PATH:$HOMEBREW_BIN/bin"
-
-# === CLI tools === #
-brew install fish \
-  node \
-  rg \
-  jq \
-  jsonlint \
-  fzf \
-  htop \
-  docker \
-  shellcheck \
-  stow \
-  tmux \
-  yabai \
-  skhd \
-  universal-ctags
-
-brew install --HEAD neovim \ # nightly build
-
-# === Applications === #
-brew install --cask firefox \
-  google-chrome \
-  rectangle
-
-# === Fonts === #
-brew tap homebrew/cask-fonts
-brew install font-fira-code
+# common setup that only needs to execute once
 
 # === Other installations === #
 # npm global installs
@@ -50,12 +12,4 @@ npm install -g yarn \
 defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false 
 defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false 
 defaults delete -g ApplePressAndHoldEnabled     
-
-# Change default shell
-echo "$HOMEBREW_BIN/fish" | sudo tee -a /etc/shell
-chsh -s "$HOMEBREW_BIN/fish"
-
-# Final step: restart shell so it launches fish
-# Run the following command to setup fish path
-# set -U fish_user_paths "$HOMEBREW_BIN" $fish_user_paths
 
