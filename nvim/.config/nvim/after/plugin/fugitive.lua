082e61aaecd1a6d79325c5a0dd391dad1993844f
Sam Dhondt <sam.dhondt@codifly.be>
2025-11-19 13:15:26 +0100

    (nvim) Remove unused git fugitive mappings

--- nvim/.config/nvim/after/plugin/fugitive.lua
+++ nvim/.config/nvim/after/plugin/fugitive.lua
@@ -5,7 +5,3 @@ vim.keymap.set('n', '<Leader>g<Right>', '<Cmd>Git rebase --continue<CR>')
 -- rebase on main, signing all commits
 vim.keymap.set('n', '<Leader>gs', '<Cmd>Git rebase --exec "git commit --amend --no-edit -n -S" -i origin/main<CR>')
 
-vim.keymap.set('n', '<Leader>gcc', '<Cmd>Git commit -vsS<CR>')
-vim.keymap.set('n', '<Leader>gcn', '<Cmd>Git commit -vsS -n<CR>')
-vim.keymap.set('n', '<Leader>gca', '<Cmd>Git commit -vsS --amend<CR>')
-vim.keymap.set('n', '<Leader>gcA', '<Cmd>Git commit -vsS --amend --no-edit<CR>')
