23398d71b3e22c85f342a285ed9cd4f9927ce688
Sam Dhondt <sam.dhondt@codifly.be>
2025-11-19 13:14:38 +0100

    (nvim) Change default telescope command to git_files

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,5 +1,5 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<Leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<Leader>ff', builtin.git_files, {})
 vim.keymap.set('n', '<Leader>fw', builtin.grep_string, {})
 vim.keymap.set('n', '<Leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<Leader>fb', builtin.buffers, {})
